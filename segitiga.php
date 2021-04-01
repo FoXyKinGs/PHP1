476d32b0be2bd88b6cbdaa52e540d59a58d574b7
FoXyKinGs <mochrizaldianandakurnia21@gmail.com>
2021-04-01 22:51:17 +0700

    validation

--- segitiga.php
+++ segitiga.php
@@ -1,11 +1,16 @@
 <?php 
 
 function segitiga($param){
-  for($i = $param; $i >= 1; $i--){
-    for($j = 1; $j <= $i; $j++){
-      echo $j;
+  if(gettype($param) != 'integer') {
+    echo "Data harus number";
+  }
+  else {
+    for($i = $param; $i >= 1; $i--){
+      for($j = 1; $j <= $i; $j++){
+        echo $j;
+      }
+    echo "\n";
     }
-  echo "\n";
   }
 }
 
